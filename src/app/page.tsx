19aab94f8978116a870cf53ceebbc56adfd94816
codeReaper <boluwatifetella@gmail.com>
2023-11-13 12:30:10 +0100

    implementing next auth

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+import Button from "@/components/ui/Button";
+
+export default function Home() {
+  return (
+    <main className="">
+      Hi
+      <Button>HI </Button>
+    </main>
+  );
+}
